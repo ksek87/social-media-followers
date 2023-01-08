b9ab47c102b44346b59bee60620026b5dce53f3c
Keelin Sekerka <59572821+ksek87@users.noreply.github.com>
2023-01-08 17:32:57 -0400

    initial skeleton

---
+++ src/main.py
@@ -0,0 +1,8 @@
+"""
+File: social-media-followers/main.py
+Program Description:
+    This script will provide a user with the list accounts that do not follow them back on social media. This can be
+    used to track who's unfollowing you, or to help you clear out your social media feed.
+Author: @Keelin Sekerka
+"""
+
